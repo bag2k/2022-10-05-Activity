c732e610fd45a87b4479c80018c91bb5c6cd0e38
bag2k <104628170+bag2k@users.noreply.github.com>
2022-10-10 08:17:03 +0800

    initial commit

--- Main.java
+++ Main.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.*;
-public class Main {
+import java.util.ArrayList;
+
+public class Mymain{
     public static void main(String[] args){
-        List<Student> students = new ArrayList<Student>();
-        List<Student> student = new ArrayList<Student>();
-
-        String firstname, Secondname;
         Scanner scan = new Scanner(System.in);
+        ArrayList<Names> names = new ArrayList<Names>();
+            System.out.print("Enter number/s of person you want to add: ");
+            int obj = scan.nextInt();
+            scan.nextLine();
+                try{
+                        if(obj > 0 ){
+                            for(int i = 0;i<obj; i++){
+                                System.out.println("Enter the name of " + i + ": ");
+                                String name = scan.nextLine();
+                                names.add(new Names(name));
+                            }
+                            for(int j = 0;j<obj; j++){
+                                System.out.println(names.get(j).getName());
+                            }
+                        }else if(obj == 0){
+                            scan.close();
+                            throw new ExceptionS("Zero is not valid to input");
+                        }else{
+                            scan.close();
+                            throw new ExceptionS("Negative Variable is not allowed");
+                        }
+                }catch(ExceptionS e){
+                    System.out.println(e.getMessage());
+                }
         
-        try{
-            System.out.println("Enter 1st student's name: ");
-            firstname = scan.nextLine();
-            System.out.println("Enter 2st student's name: ");
-            Secondname = scan.nextLine();
-            System.out.println("Enter 1st student's age: ");
-            int firstage = scan.nextInt();
-            System.out.println("Enter 2st student's age: ");
-            int Secondage = scan.nextInt();
-            
-            students.add(new Student(firstname, firstage));
-            student.add(new Student(Secondname, Secondage));
-
-              
-            System.out.println(students);
-            System.out.println(student);
-
-                // System.out.println("My name is " + firstname + ". "+ firstage + " years old");
-                // System.out.println("My name is " + Secondname + ". "+ Secondage + " years old");
-        }
-        catch(Exception e){
-            System.out.println("nothing's here");
-        }scan.close();
-     
-    }
+    } 
 }
